e14454f7af0117e0b1af49918eda9d797479ba7b
ttttttwt <100847404+ttttttwt@users.noreply.github.com>
2023-09-19 10:23:02 +0700

    new update

---
+++ src/main/java/com/example/chuyen_de_1/ChuyenDe1Application.java
@@ -0,0 +1,13 @@
+package com.example.chuyen_de_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuyenDe1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuyenDe1Application.class, args);
+	}
+
+}
